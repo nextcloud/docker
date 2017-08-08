ce5b8148861a64f5b840ec93c3eb38d1c3f7b6e5
Tilo Spannagel <development@tilosp.de>
2017-08-08 16:46:36 +0200

    Add build tests for the example dockerfiles

---
+++ .travis/test-example-dockerfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+image="$1"
+
+cd .examples/dockerfiles
+
+dirs=( */ )
+dirs=( "${dirs[@]%/}" )
+for dir in "${dirs[@]}"; do
+    if [ -d "$dir/$VARIANT" ]; then
+        (
+            cd "$dir/$VARIANT" 
+            sed -ri -e 's/^FROM .*/FROM '"$image"'/g' 'Dockerfile'
+            docker build -t "$image-$dir" .
+            ~/official-images/test/run.sh "$image-$dir"
+        )
+    fi
+done
