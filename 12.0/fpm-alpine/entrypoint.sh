1654a164dc1a7f6e01f418dcf8b5e2c48f08f708
Tilo Spannagel <development@tilosp.de>
2018-08-16 18:10:27 +0200

    Run update.sh
    Signed-off-by: Tilo Spannagel <development@tilosp.de>

--- 12.0/fpm-alpine/entrypoint.sh
+++ 12.0/fpm-alpine/entrypoint.sh
@@ -50,7 +50,7 @@ if version_greater "$image_version" "$installed_version"; then
     done
 
     if [ "$installed_version" != "0.0.0.0" ]; then
-        run_as 'php /var/www/html/occ upgrade --no-app-disable'
+        run_as 'php /var/www/html/occ upgrade'
 
         run_as 'php /var/www/html/occ app:list' | sed -n "/Enabled:/,/Disabled:/p" > /tmp/list_after
         echo "The following apps have beed disabled:"
