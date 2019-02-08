21e5421e75cdf997392d24e03407813ce8535b84
marceljd <support@dihosting.ch>
2019-02-08 04:49:32 +0000

    On branch apache-ssl  Changes to be committed: 	modified:   13.0/apache/entrypoint.sh 	modified:   13.0/fpm-alpine/entrypoint.sh 	modified:   13.0/fpm/entrypoint.sh 	modified:   14.0/apache/entrypoint.sh 	modified:   14.0/fpm-alpine/entrypoint.sh 	modified:   14.0/fpm/entrypoint.sh 	modified:   15.0/apache/entrypoint.sh 	modified:   15.0/fpm-alpine/entrypoint.sh 	modified:   15.0/fpm/entrypoint.sh 	modified:   docker-entrypoint.sh
    Signed-off-by: marceljd <support@dihosting.ch>

--- 13.0/fpm/entrypoint.sh
+++ 13.0/fpm/entrypoint.sh
@@ -135,7 +135,7 @@ echo "net voor de loop"
 
 
 # if expr "$1" : "apache" 1>/dev/null && [ -n "${APACHE_SSL_SELFSIGNED+x}" ] ; then
-if [ ! "/etc/apache2"]; then
+if [ -d "/etc/apache2"]; then
   echo "in de apache loop"	
   a2enmod ssl
   a2enmod headers
