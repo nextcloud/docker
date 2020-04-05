83255f2385366553d47a9409a3696ff64778057e
András Maróy <andras@maroy.hu>
2020-04-05 15:35:28 +0200

    Change reverse proxy default values to null
    Signed-off-by: András Maróy <andras@maroy.hu>

--- 17.0/fpm-alpine/config/reverse-proxy.config.php
+++ 17.0/fpm-alpine/config/reverse-proxy.config.php
@@ -2,29 +2,29 @@
 if (getenv('OVERWRITEHOST')) {
   $CONFIG['overwritehost'] = getenv('OVERWRITEHOST');
 } else {
-  $CONFIG['overwritehost'] = '';
+  $CONFIG['overwritehost'] = null;
 }
 
 if (getenv('OVERWRITEPROTOCOL')) {
   $CONFIG['overwriteprotocol'] = getenv('OVERWRITEPROTOCOL');
 } else {
-  $CONFIG['overwriteprotocol'] = '';
+  $CONFIG['overwriteprotocol'] = null;
 }
 
 if (getenv('OVERWRITEWEBROOT')) {
   $CONFIG['overwritewebroot'] = getenv('OVERWRITEWEBROOT');
 } else {
-  $CONFIG['overwritewebroot'] = '';
+  $CONFIG['overwritewebroot'] = null;
 }
 
 if (getenv('OVERWRITECONDADDR')) {
   $CONFIG['overwritecondaddr'] = getenv('OVERWRITECONDADDR');
 } else {
-  $CONFIG['overwritecondaddr'] = '';
+  $CONFIG['overwritecondaddr'] = null;
 }
 
 if (getenv('TRUSTED_PROXIES')) {
   $CONFIG['trusted_proxies'] = explode(',', getenv('TRUSTED_PROXIES'));
 } else {
-  $CONFIG['trusted_proxies'] = [];
+  $CONFIG['trusted_proxies'] = null;
 }
