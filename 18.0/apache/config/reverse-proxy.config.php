2b2bd3c6aa4d736a71902d555bf93b7f8b973c89
András Maróy <andras@maroy.hu>
2020-04-05 15:16:49 +0200

    Add default values for reverse proxy config from environment
    Signed-off-by: András Maróy <andras@maroy.hu>

--- 18.0/apache/config/reverse-proxy.config.php
+++ 18.0/apache/config/reverse-proxy.config.php
@@ -1,20 +1,30 @@
 <?php
 if (getenv('OVERWRITEHOST')) {
   $CONFIG['overwritehost'] = getenv('OVERWRITEHOST');
+} else {
+  $CONFIG['overwritehost'] = '';
 }
 
 if (getenv('OVERWRITEPROTOCOL')) {
   $CONFIG['overwriteprotocol'] = getenv('OVERWRITEPROTOCOL');
+} else {
+  $CONFIG['overwriteprotocol'] = '';
 }
 
 if (getenv('OVERWRITEWEBROOT')) {
   $CONFIG['overwritewebroot'] = getenv('OVERWRITEWEBROOT');
+} else {
+  $CONFIG['overwritewebroot'] = '';
 }
 
 if (getenv('OVERWRITECONDADDR')) {
   $CONFIG['overwritecondaddr'] = getenv('OVERWRITECONDADDR');
+} else {
+  $CONFIG['overwritecondaddr'] = '';
 }
 
 if (getenv('TRUSTED_PROXIES')) {
   $CONFIG['trusted_proxies'] = explode(',', getenv('TRUSTED_PROXIES'));
+} else {
+  $CONFIG['trusted_proxies'] = [];
 }
