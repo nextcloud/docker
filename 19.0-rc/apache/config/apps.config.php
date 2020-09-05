1e2c9961f7a42b864a1ba99527d82671eee78245
GitHub Workflow <workflow@github.com>
2020-09-05 01:46:19 +0000

    Runs update.sh

---
+++ 19.0-rc/apache/config/apps.config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  "apps_paths" => array (
+      0 => array (
+              "path"     => OC::$SERVERROOT."/apps",
+              "url"      => "/apps",
+              "writable" => false,
+      ),
+      1 => array (
+              "path"     => OC::$SERVERROOT."/custom_apps",
+              "url"      => "/custom_apps",
+              "writable" => true,
+      ),
+  ),
+);
