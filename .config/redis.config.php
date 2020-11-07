b8cc63fb83d567184cfd54a82df5b2af58ca018f
stefanprobst <stefan.probst@univie.ac.at>
2020-11-07 01:36:05 +0100

    fix: empty redis password must be string (#1232)

--- .config/redis.config.php
+++ .config/redis.config.php
@@ -1,11 +1,11 @@
 <?php
 if (getenv('REDIS_HOST')) {
-  $CONFIG = array (
+  $CONFIG = array(
     'memcache.distributed' => '\OC\Memcache\Redis',
     'memcache.locking' => '\OC\Memcache\Redis',
     'redis' => array(
       'host' => getenv('REDIS_HOST'),
-      'password' => getenv('REDIS_HOST_PASSWORD'),
+      'password' => (string) getenv('REDIS_HOST_PASSWORD'),
     ),
   );
 
