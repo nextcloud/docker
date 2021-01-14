6a02b60ce2b7e77aad13ef75ce679ea0dcaaaad7
Alexander Sosna <alexander.sosna@credativ.de>
2021-01-14 18:53:04 +0100

    Run update.sh

--- 18.0/apache/entrypoint.sh
+++ 18.0/apache/entrypoint.sh
@@ -43,6 +43,13 @@ file_env() {
     unset "$fileVar"
 }
 
+# Get essential PHP_INI configuration
+file_env PHP_INI_MEMORY_LIMIT 512M
+
+# Set essential PHP_INI configuration
+# It is important to set essential variables like 'memory-limit' before the first start to premvent OOM during installation and statup.
+echo "memory_limit=${PHP_INI_MEMORY_LIMIT}" > /usr/local/etc/php/conf.d/memory-limit.ini;
+
 if expr "$1" : "apache" 1>/dev/null; then
     if [ -n "${APACHE_DISABLE_REWRITE_IP+x}" ]; then
         a2disconf remoteip
