6ef8de9c85a010d1b5d31377a247d9b45c2a15f2
GitHub Workflow <workflow@github.com>
2021-02-23 01:09:34 +0000

    Runs update.sh

--- 20.0/fpm-alpine/config/s3.config.php
+++ 20.0/fpm-alpine/config/s3.config.php
@@ -2,6 +2,7 @@
 if (getenv('OBJECTSTORE_S3_BUCKET')) {
   $use_ssl = getenv('OBJECTSTORE_S3_SSL');
   $use_path = getenv('OBJECTSTORE_S3_USEPATH_STYLE');
+  $autocreate = getenv('OBJECTSTORE_S3_AUTOCREATE');
   $CONFIG = array(
     'objectstore' => array(
       'class' => '\OC\Files\ObjectStore\S3',
@@ -12,6 +13,8 @@ if (getenv('OBJECTSTORE_S3_BUCKET')) {
         'region' => getenv('OBJECTSTORE_S3_REGION') ?: '',
         'hostname' => getenv('OBJECTSTORE_S3_HOST') ?: '',
         'port' => getenv('OBJECTSTORE_S3_PORT') ?: '',
+        'objectPrefix' => getenv("OBJECTSTORE_S3_OBJECT_PREFIX") ? getenv("OBJECTSTORE_S3_OBJECT_PREFIX") : "urn:oid:",
+        'autocreate' => (strtolower($autocreate) === 'false' || $autocreate == false) ? false : true,
         'use_ssl' => (strtolower($use_ssl) === 'false' || $use_ssl == false) ? false : true,
         // required for some non Amazon S3 implementations
         'use_path_style' => $use_path == true && strtolower($use_path) !== 'false'
