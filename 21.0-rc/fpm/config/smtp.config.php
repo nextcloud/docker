bd1b16613d0eae96d67bad4c6cb0914f4b99956b
András Maróy <andras@maroy.hu>
2021-04-08 20:59:24 +0200

    Implement docker secret handling for SMTP_PASSWORD
    Signed-off-by: András Maróy <andras@maroy.hu>

--- 21.0-rc/fpm/config/smtp.config.php
+++ 21.0-rc/fpm/config/smtp.config.php
@@ -8,8 +8,15 @@ if (getenv('SMTP_HOST') && getenv('MAIL_FROM_ADDRESS') && getenv('MAIL_DOMAIN'))
     'mail_smtpauth' => getenv('SMTP_NAME') && getenv('SMTP_PASSWORD'),
     'mail_smtpauthtype' => getenv('SMTP_AUTHTYPE') ?: 'LOGIN',
     'mail_smtpname' => getenv('SMTP_NAME') ?: '',
-    'mail_smtppassword' => getenv('SMTP_PASSWORD') ?: '',
     'mail_from_address' => getenv('MAIL_FROM_ADDRESS'),
     'mail_domain' => getenv('MAIL_DOMAIN'),
   );
+
+  if (getenv('SMTP_PASSWORD_FILE') && file_exists(getenv('SMTP_PASSWORD_FILE'))) {
+      $CONFIG['mail_smtppassword'] = rtrim(file_get_contents(getenv('SMTP_PASSWORD_FILE')));
+  } elseif (getenv('SMTP_PASSWORD')) {
+      $CONFIG['mail_smtppassword'] = getenv('SMTP_PASSWORD');
+  } else {
+      $CONFIG['mail_smtppassword'] = '';
+  }
 }
