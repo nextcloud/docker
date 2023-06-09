ab4b43c93b8876e19b13e7a9c9bce08f96801278
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2023-06-09 11:49:51 +0200

    Remove prints, add timeouts

--- nextcloud_install_playwright.py
+++ nextcloud_install_playwright.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import sync_playwright
-from time import time
 
 def main():
     with sync_playwright() as playwright:
@@ -16,17 +15,14 @@ def main():
         page.type('#adminlogin', 'Crash')
         page.type('#adminpass', 'Override')
         page.click('.primary')
-        print(time(), "Create user clicked")
 
         # 2. Install all Apps
         install_selector = '.button-vue--vue-primary'
         page.wait_for_selector(install_selector)
         page.click(install_selector)
-        print(time(), "Install apps clicked")
 
         # 3. Dashboard
         page.wait_for_selector('.app-dashboard')
-        print(time(), "Dashboard found")
 
         browser.close()
 
