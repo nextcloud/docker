6a5c1165193da476e3e6dd2eb360684fadedbd36
Danilo Ješić <34022788+djesic@users.noreply.github.com>
2023-06-16 17:02:47 +0200

    Write and validate longer text. Added notes

--- nextcloud_docs_collaboration.py
+++ nextcloud_docs_collaboration.py
@@ -1,6 +1,21 @@
+import random
+import string
+from time import time_ns, sleep
+
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+
+def get_random_text() -> str:
+    size_in_bytes = 1024
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(size_in_bytes))
+
+def log_note(message: str) -> None:
+    timestamp = round(time_ns() * 1000)
+    print(f"{timestamp} {message}")
+
 def collaborate(playwright: Playwright) -> None:
+    log_note("Launching browsers")
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     admin_user = context.new_page()
@@ -10,8 +25,10 @@ def collaborate(playwright: Playwright) -> None:
     docs_user = context_two.new_page()
 
     # Login and open the file for both users
+    log_note("Logging in with users")
     login(admin_user, "Crash", "Override")
     login(docs_user, "docs_dude", "docsrule!12")
+    log_note("Opening document with both users")
     docs_user.get_by_role("button", name="Close modal").click(timeout=15_000)
     admin_user.get_by_role("link", name="Files").click()
     docs_user.get_by_role("link", name="Files").click()
@@ -21,14 +38,27 @@ def collaborate(playwright: Playwright) -> None:
     docs_user.get_by_role("link", name="colab_meeting .md").click()
 
     # Write the first message and assert it's visible for the other user
-    first_message = "FIRST_TEST_MESSAGE"
+    log_note("Sending first validation message")
+    first_message = "FIRST_VALIDATION_MESSAGE"
     admin_user.get_by_role("dialog", name="colab_meeting.md").get_by_role("document").locator("div").first.type(first_message)
     expect(docs_user.get_by_text(first_message)).to_be_visible()
 
-    second_message = " some other text"
-    docs_user.get_by_role("dialog", name="colab_meeting.md").get_by_role("document").locator("div").first.type(second_message)
-    expect(admin_user.get_by_text(second_message)).to_be_visible()
+    for x in range(1, 7):
+        random_message = get_random_text()
+        # Admin sends on even, docs_dude on odd
+        if x % 2 == 0:
+            log_note("Admin adding text")
+            admin_user.get_by_role("dialog", name="colab_meeting.md").get_by_role("document").locator("div").first.type(random_message)
+            expect(docs_user.get_by_text(random_message)).to_be_visible()
+        else:
+            log_note("User adding text")
+            docs_user.get_by_role("dialog", name="colab_meeting.md").get_by_role("document").locator("div").first.type(random_message)
+            expect(admin_user.get_by_text(random_message)).to_be_visible()
 
+        log_note("Sleeping for 10 seconds")
+        sleep(10)
+
+    log_note("Closing browsers")
     # ---------------------
     admin_user.close()
     docs_user.close()
